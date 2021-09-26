cca70390eba69c82f9181011f3406d82775b92c6
linggeg1 <linggeg1@student.unimelb.edu.au>
2021-09-26 23:11:13 +0800

    make question in questionare Question

--- src/main/java/com/example/Flying_Tiger/Questionaire.java
+++ src/main/java/com/example/Flying_Tiger/Questionaire.java
@@ -6,32 +6,52 @@ public class Questionaire {
     private long ID;
     private String vacType;
     private long hcpID;
-    private String q1;
-    private String q2;
-    private String q3;
-    private String q4;
-    private String q5;
-    public Questionaire(long ID, String vacType, long hcpID, String q1, String q2, String q3, String q4, String q5){
+    private Question q1;
+    private Question q2;
+    private Question q3;
+    private Question q4;
+    private Question q5;
+    private int minAge;
+    private int maxAge;
+    public Questionaire(long ID, String vacType, long hcpID,int minAge,int maxAge, String q1, String q2,
+                        String q3, String q4, String q5){
         this.ID = ID;
         this.vacType = vacType;
         this.hcpID = hcpID;
-        this.q1 = q1;
-        this.q2 = q2;
-        this.q3 = q3;
-        this.q4 = q4;
-        this.q5 = q5;
-
+        this.q1 = new Question(q1);
+        this.q2 = new Question(q2);
+        this.q3 = new Question(q3);
+        this.q4 = new Question(q4);
+        this.q5 = new Question(q5);
+        this.minAge=minAge;
+        this.maxAge=maxAge;
     }
     public String[] getQuestions() throws SQLException {
         if(this.q1 == null || this.q2 == null || this.q3 == null || this.q4 == null || this.q5 == null){
             load();
         }
-        String[] questions = {this.q1, this.q2, this.q3, this.q4, this.q5};
+        String[] questions = {this.q1.getQuestionContent(), this.q2.getQuestionContent(), this.q3.getQuestionContent(), this.q4.getQuestionContent(), this.q5.getQuestionContent()};
         return questions;
     }
 
     public long getHcpID() {
         return hcpID;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
     }
 
     public long getID() {
@@ -44,13 +64,13 @@ public class Questionaire {
 
     public String getQ1() {
         if(q1!=null)
-            return q1;
+            return q1.getQuestionContent();
             else
         {
             try {
                 Questionaire q = this.getMapper().find(this.ID);
                 this.setQ1(q.getQ1());
-                return this.q1;
+                return this.q1.getQuestionContent();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -59,13 +79,13 @@ public class Questionaire {
     }
     public String getQ2() {
         if(q2!=null)
-            return q2;
+            return q2.getQuestionContent();
         else
         {
             try {
                 Questionaire q = this.getMapper().find(this.ID);
                 this.setQ2(q.getQ2());
-                return this.q2;
+                return this.q2.getQuestionContent();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -74,13 +94,13 @@ public class Questionaire {
     }
     public String getQ3() {
         if(q3!=null)
-            return q3;
+            return q3.getQuestionContent();
         else
         {
             try {
                 Questionaire q = this.getMapper().find(this.ID);
                 this.setQ3(q.getQ3());
-                return this.q3;
+                return this.q3.getQuestionContent();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -89,13 +109,13 @@ public class Questionaire {
     }
     public String getQ4() {
         if(q4!=null)
-            return q4;
+            return q4.getQuestionContent();
         else
         {
             try {
                 Questionaire q = this.getMapper().find(this.ID);
                 this.setQ4(q.getQ4());
-                return this.q4;
+                return this.q4.getQuestionContent();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -104,13 +124,13 @@ public class Questionaire {
     }
     public String getQ5() {
         if(q5!=null)
-            return q5;
+            return q5.getQuestionContent();
         else
         {
             try {
                 Questionaire q = this.getMapper().find(this.ID);
                 this.setQ5(q.getQ5());
-                return this.q5;
+                return this.q5.getQuestionContent();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -118,23 +138,23 @@ public class Questionaire {
         return null;
     }
     public void setQ1(String q1) {
-        this.q1 = q1;
+        this.q1 = new Question(q1);
     }
 
     public void setQ2(String q2) {
-        this.q2 = q2;
+        this.q2 = new Question(q2);
     }
 
     public void setQ3(String q3) {
-        this.q3 = q3;
+        this.q3 = new Question(q3);
     }
 
     public void setQ4(String q4) {
-        this.q4 = q4;
+        this.q4 = new Question(q4);
     }
 
     public void setQ5(String q5) {
-        this.q5 = q5;
+        this.q5 = new Question(q5);
     }
 
     /**
@@ -143,11 +163,11 @@ public class Questionaire {
      */
     void load() throws SQLException {
         Questionaire q = this.getMapper().find(this.ID);
-        if(this.q1 == null) this.q1 = q.getQ1();
-        if(this.q2 == null) this.q2 = q.getQ2();
-        if(this.q3 == null) this.q3 = q.getQ3();
-        if(this.q4 == null) this.q4 = q.getQ4();
-        if(this.q5 == null) this.q5 = q.getQ5();
+        if(this.q1 == null) this.q1 = new Question(q.getQ1());
+        if(this.q2 == null) this.q2 = new Question(q.getQ2());
+        if(this.q3 == null) this.q3 = new Question(q.getQ3());
+        if(this.q4 == null) this.q4 = new Question(q.getQ4());
+        if(this.q5 == null) this.q5 = new Question(q.getQ5());
     }
     public static QuestionaireMapper getMapper()
     {
