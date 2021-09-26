af1b717aa0806d9c34bd8be0fae2287e8d7bbf44
ZhimingDeng5 <zhimingd@student.unimelb.edu.au>
2021-09-27 01:53:19 +0800

    1

--- src/main/java/com/example/Flying_Tiger/Question.java
+++
@@ -1,9 +0,0 @@
-package com.example.Flying_Tiger;
-
-public class Question {
-    private String questionContent;
-    public Question(String questionContent){
-        this.questionContent = questionContent;
-    }
-    public String getQuestionContent() {return questionContent; }
-}
--- src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="refresh" content="1;url=src/ui-elements/auth-signin.jsp">
-</head>
-
-</html>
